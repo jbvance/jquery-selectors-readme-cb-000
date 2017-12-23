3c4177d0b312bb1fa5e7640daee140e3d62bebfa
Jason Vance <jbvance@gmail.com>
2017-12-23 11:39:35 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,18 +1,18 @@
 // declare your functions here...
 function paragraphSelector() {
-  $('p');
+  return $('p');
 }
 
 function lastImageSelector() {
-  $('img:last');
+  return $('img:last');
 }
 
 function ninjaBabySelector() {
-  $('#baby-ninja');
+  return $('#baby-ninja');
 }
 
 function divSelector() {
-  $('.pics');
+  return $('.pics');
 }
 
 function firstListItem() {}
